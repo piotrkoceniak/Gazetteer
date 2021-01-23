b42362d03583c79d4b7bcfa2d7bd5495a5554e11
piotrkoceniak <70285061+piotrkoceniak@users.noreply.github.com>
2021-01-23 11:33:44 +0000

    map first layer set

---
+++ js/map.js
@@ -0,0 +1,6 @@
+var mymap = L.map('mapid').setView([45.00, 0.00], 2.5);
+
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+    maxZoom: 17,
+}).addTo(mymap);
