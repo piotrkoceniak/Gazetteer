9e40d5329b8bb5d06196f353f177dea90168797a
piotrkoceniak <70285061+piotrkoceniak@users.noreply.github.com>
2021-01-26 16:02:28 +0000

    map controls - tiles

--- js/map.js
+++ js/map.js
@@ -12,10 +12,9 @@ var mapnikMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
-var stadiaMap = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-});
+
+// adding layers control
+L.control.layers({"OpenTopoMap": topoMap, "OpenSteetMap": mapnikMap}).addTo(mymap);
 
 // setting up popups - details
 var detailsPopup = L.popup();
