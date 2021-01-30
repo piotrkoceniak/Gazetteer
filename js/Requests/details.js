598aeb6538d7f706ad2c224d4559f6ae6e27a280
piotrkoceniak <70285061+piotrkoceniak@users.noreply.github.com>
2021-01-30 17:39:10 +0000

    details applied, no weather

--- js/Requests/details.js
+++ js/Requests/details.js
@@ -16,7 +16,29 @@ function getCountryDetails(countryCode) {
 }
 
 function handleDetailsResponse(response) {
+    console.log("Received");
     console.log(response);
+
+    $("#c-name").html("Details - " + response.data.details.countryName);
+    $("#full-c-name").html(response.data.fullName);
+    $("#c-population").html(response.data.details.population);
+    $("#c-area").html(response.data.details.areaInSqKm + " km<sup>2</sup>");
+    $("#c-currency").html(response.data.details.currencyCode);
+    $("#c-neighbours").html(returnAsString(response, "neighbours"));
+    $("#c-continent").html(response.data.details.continentName);
+    $("#c-capital").html(response.data.details.capital);
+    //$("#c-current-weather").html(response);
+    $("#c-cities").html(returnAsString(response, "cities"));
+    $("#c-postal-code").html(response.data.details.postalCodeFormat);
+    console.log("Applied"); 
+}
+
+function returnAsString(response, key) {
+    let string = "";
+    response.data[key].forEach(element => {
+        string += string ? (", " + element.name) : element.name;
+    });
+    return string;
 }
 
 
--- php/details.php
+++ php/details.php
@@ -6,9 +6,9 @@
     
     // fetching details
     $detailsUrl = "http://api.geonames.org/countryInfoJSON?lang=en&country=".$_REQUEST["country"]."&username=pkoceniak";
-
+    $countryNameUrl = "http://api.geonames.org/searchJSON?q=&country=".$_REQUEST["country"]."&featureCode=PCLI&username=pkoceniak";
     $details = sendRequest($detailsUrl);
-    
+    $fullName = sendRequest($countryNameUrl);
     
     // getting neighbours
     $neighboursUrl = "http://api.geonames.org/neighboursJSON?geonameId=".$details["geonames"][0]["geonameId"]."&username=pkoceniak";
@@ -27,6 +27,7 @@
     $responseData["details"] = $details["geonames"][0];
     $responseData["neighbours"] = $neighbours["geonames"];
     $responseData["cities"] = $cities["geonames"];
+    $responseData["fullName"] = $fullName["geonames"][0]["toponymName"];
     sendResponse("ok", $responseData);
 
 ?>
