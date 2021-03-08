4a020a7a804c5c0f009023f61b63c7dc102f9f12
Piotr Koceniak <piotrkoceniak@yahoo.com>
2021-03-08 07:14:18 +0000

    some styles applied

--- js/Requests/news.js
+++ js/Requests/news.js
@@ -24,16 +24,16 @@ function handleNewsResponse(response) {
     let index = 0;
 
     response.data.articles.forEach(article => {
-        let indicator = `<button type="button" data-bs-target="#news-carousel" data-bs-slide-to="${index}" aria-label="News ${index}" ${index === 0 ? 'class="active"' : ''}></button>`;
+        let indicator = `<button type="button" class="mx-1" data-bs-target="#news-carousel" data-bs-slide-to="${index}" aria-label="News ${index}" ${index === 0 ? 'class="active"' : ''}></button>`;
     
         indicators += indicator;
         let imageUrl = article.urlToImage === null ? "./images/news-no-image.svg" : article.urlToImage;
         imageUrl = imageUrl.split(',');
 
-        let image = `<img src="${imageUrl[0]}" alt="News image not available" class="d-block w-100">`;
+        let image = `<img src="${imageUrl[0]}" alt="News image not available" class="img-fluid mx-auto d-block w-auto" onerror="this.src='./images/news-no-image.svg'">`;
         let heading = `<p>${article.title}</p>`;
         let paragraph = `<p>Author: ${article.author ? article.author : 'Not specified'}, Source: <a href="${article.url}">${article.source.name}</a></p>`;
-        let item = `<div class="carousel-item ${index === 0 ? 'active' : ''}">${image}<div class="carousel-caption position-relative bottom-0 start-0 bg-dark mb-4">${heading}${paragraph}</div></div>`;
+        let item = `<div class="text-center h-100 carousel-item ${index === 0 ? 'active' : ''}">${image}<div class="carousel-caption position-relative bottom-0 start-0 bg-dark mb-4">${heading}${paragraph}</div></div>`;
 
         carouselItems += item;
         index++;
--- js/Requests/pictures.js
+++ js/Requests/pictures.js
@@ -18,7 +18,6 @@ function getPictures(countryName) {
 }
 
 function handlePicturesResponse(response) {
-    console.log(response);
     $("#pictures-indicators").empty();
     $("#pictures-carousel-inner").empty();
     let indicators = "";
@@ -26,15 +25,15 @@ function handlePicturesResponse(response) {
     let index = 0;
 
     response.data.photos.photo.forEach(photo => {
-        let indicator = `<button type="button" data-bs-target="#pictures-carousel" data-bs-slide-to="${index}" aria-label="Country related picture ${index}" ${index === 0 ? 'class="active"' : ''}></button>`;
+        let indicator = `<button type="button" class="mx-1" data-bs-target="#pictures-carousel" data-bs-slide-to="${index}" aria-label="Country related picture ${index}" ${index === 0 ? 'class="active"' : ''}></button>`;
     
         indicators += indicator;
 
         let imageUrl = `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_z.jpg`;
-        let image = `<img src="${imageUrl}" alt="Image not available" class="d-block w-100">`;
+        let image = `<img src="${imageUrl}" alt="Image not available" class="img-fluid mx-auto d-block w-auto" onerror="this.src='./images/news-no-image.svg'">`;
         let heading = `<p id="country-image-heading-${index}"></p>`;
         let paragraph = `<p id="country-image-paragraph-${index}"></p>`;
-        let item = `<div class="carousel-item ${index === 0 ? 'active' : ''}">${image}<div class="carousel-caption position-relative bottom-0 start-0 bg-dark mb-4">${heading}${paragraph}</div></div>`;
+        let item = `<div class="text-center h-100 carousel-item ${index === 0 ? 'active' : ''}">${image}<div class="carousel-caption position-relative bottom-0 start-0 bg-dark mb-4">${heading}${paragraph}</div></div>`;
 
         carouselItems += item;
         index++;
