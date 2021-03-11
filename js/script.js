65e5717bc2fff0faf69d9aa0ce055e8e5b655836
Piotr Koceniak <piotrkoceniak@yahoo.com>
2021-03-11 01:33:19 +0000

    ExtraMarkers, rearranged files, some comments

--- js/script.js
+++ js/script.js
@@ -6,6 +6,9 @@ import {getPopulationDetails} from "./Requests/population-details.js";
 import {getCurrencyDetails} from "./Requests/currency-details.js";
 import {setPOIsMarkers} from "./Requests/POIs.js";
 
+console.log("Script loaded");
+
+// centering map on users location if user's browser allows it
 let location = window.navigator.geolocation;
 
 location.getCurrentPosition(
@@ -17,9 +20,11 @@ location.getCurrentPosition(
     }
 );
 
-console.log("Script loaded");
+
+// setting countries list in select
 getCountries("");
 
+// setting markers on map if map is zoomed enouch
 mymap.on('zoomstart', function() {
     let zoomLevel = mymap.getZoom();
     if(zoomLevel >= 15) {
@@ -45,37 +50,36 @@ $("#currency-close-button").click(function() {
 });
 
 
-// search events
+// select change event - getting and setting country on map
 $("#search").on("change", function (e) {
     getCountries(e.target.value);
 });
 
-$("#search-icon").click(function() {
-    $("#search").triggerHandler("keypress", true);
-});
-
-
+// opening popup on click - getting country and region from geocodes and setting it up on map and pop up
 mymap.on("click", (e) => {
     getCountryFromGeocodes(e.latlng);
 	detailsPopup.setLatLng(e.latlng).openOn(mymap);
 });
 
-// opening buttons
+// opening modal - getting country details and setting data in modal
 $("#mapid").on("click", "#details-button", function(e) {
     getCountryDetails($("#details-button").attr("data-country"));
     $('#details-modal').modal({backdrop: 'static', show: true});
 });
 
+// in modal - getting and setting weather details for country's capital
 $("#details").on("click", "#weather-button", function(e) {
     getWeatherDetails($("#weather-button").attr("data-city-lon"), $("#weather-button").attr("data-city-lat"));
     $("#details-weather").show();
 });
 
+// in modal - getting and setting population details for country
 $("#details").on("click", "#population-button", function(e) {
     getPopulationDetails($("#population-button").attr("data-country"));
     $("#details-population").show();
 });
 
+// in modal - getting and setting currency exchange rates and gdp for a country
 $("#details").on("click", "#currency-button", function(e) {
     getCurrencyDetails($("#currency-button").attr("data-country"), $("#currency-button").attr("data-currency"));
     $("#details-currency").show();
