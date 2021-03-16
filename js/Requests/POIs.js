0586f3bd0cea8093a664ae2325f8232bf93242d3
Piotr Koceniak <piotrkoceniak@yahoo.com>
2021-03-16 02:04:48 +0000

    added a few icons to markers

--- js/Requests/POIs.js
+++ js/Requests/POIs.js
@@ -20,12 +20,43 @@ function handlePOIsResponse(response) {
     console.log(response);
     POIsMarkers.clearLayers();
     response.data.poi.forEach(point => {
-        const marker = L.marker([point.lat, point.lng]);
-        let typeNameString = point.typeName.replace('_', " ");
+        var POIMarker = L.icon({
+            iconUrl: switchIconUrls(point.typeClass, point.typeName),
+            iconAnchor: [0, 8],
+            tooltipAnchor: [24, 4]
+          });
+
+
+        const marker = L.marker([point.lat, point.lng], {icon: POIMarker});
+        let typeNameString = point.typeName.replaceAll('_', " ");
         marker.bindTooltip(`Name: ${point.name}<br/>Type: ${typeNameString}`);
         POIsMarkers.addLayer(marker);
     });
     mymap.addLayer(POIsMarkers);
+}
+
+function switchIconUrls(typeClass, typeName) {
+    let iconUrl = './images/icons/';
+    switch(typeClass) {
+        case 'amenity':
+            if(typeName === 'bicycle_rental') {
+                return iconUrl + 'bicycle.svg';
+            } else {
+                return iconUrl + 'arrow-down-square.svg';
+            }
+        case 'railway':
+            return iconUrl + 'train.svg';
+        case 'tourism':
+            if(typeName === 'hotel') {
+                return iconUrl + 'house-fill.svg';
+            } else {
+                return iconUrl + 'info-square.svg';
+            }
+        case 'shop':
+            return iconUrl + 'cart3.svg';
+        default:
+            return './css/Leaflet/images/marker-icon.png';
+    }
 }
 
 export {setPOIsMarkers};
--- php/POIsMarkers.php
+++ php/POIsMarkers.php
@@ -4,10 +4,10 @@
     include("test.php");
 
     // getting points of interest
-    $poisUrl = "http://api.geonames.org/findNearbyPOIsOSMJSON?lat=".$_REQUEST['lat']."&lng=".$_REQUEST['lng']."&maxRows=10&username=pkoceniak";
+    $poisUrl = "http://api.geonames.org/findNearbyPOIsOSMJSON?lat=".$_REQUEST['lat']."&lng=".$_REQUEST['lng']."&maxRows=20&radius=1&username=pkoceniak";
 
     $pois = sendRequest($poisUrl);
-
+    test($pois);
     // response
     sendResponse("ok", $pois);
 
